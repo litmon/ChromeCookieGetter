8c0c2314ca9f764a08dc724beadd1bc5ca481110
Fukuo Kadota <app.fk91@gmail.com>
2016-12-12 22:09:26 +0900

    Innitial commit

---
+++ app/src/main/java/com/litmon/app/chromecookiegetter/MainActivity.java
@@ -0,0 +1,23 @@
+package com.litmon.app.chromecookiegetter;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://10.0.1.2/index.html")));
+            }
+        });
+    }
+}
