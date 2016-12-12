225cc4b13d9d1644c12387cb9375737c3621d901
Fukuo Kadota <app.fk91@gmail.com>
2016-12-12 22:58:48 +0900

    get cookie from chrome custom tabs

--- app/src/main/java/com/litmon/app/chromecookiegetter/MainActivity.java
+++ app/src/main/java/com/litmon/app/chromecookiegetter/MainActivity.java
@@ -2,9 +2,15 @@ package com.litmon.app.chromecookiegetter;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.customtabs.CustomTabsClient;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.customtabs.CustomTabsService;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,11 +21,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final Uri uri = Uri.parse("http://" + IP_ADDRESS + "/index.html");
+
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://" + IP_ADDRESS + "/index.html")));
+                startActivity(new Intent(Intent.ACTION_VIEW, uri));
             }
         });
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CustomTabsIntent intent = new CustomTabsIntent.Builder()
+                        .build();
+
+                intent.launchUrl(MainActivity.this, uri);
+            }
+        });
+
+        if(getIntent().getData() != null){
+            TextView textView = (TextView) findViewById(R.id.textView);
+            textView.setText(getIntent().getDataString());
+        }
     }
 }
